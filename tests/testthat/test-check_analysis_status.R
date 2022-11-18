435eb2cf383f44269a7f8364c12253aa190aadff
Liz Roten <liz.roten@metc.state.mn.us>
2022-11-18 12:33:12 -0600

    update tests

--- tests/testthat/test-check_analysis_status.R
+++ tests/testthat/test-check_analysis_status.R
@@ -2,12 +2,13 @@ testthat::test_that("Analysis status correct", {
   status <- check_analysis_status(
     analysis_name_ = "testing4"
   ) %>%
+    httr2::resp_body_json(simplifyVector = TRUE) %>% 
     testthat::expect_warning()
 
-  avail <- status$status
-  name <- status$name
-  metrics1 <- status$metrics[[1]][1]
-  metrics2 <- status$metrics[[1]][2]
+  avail <- status$analyses$status
+  name <- status$analyses$name
+  metrics1 <- status$analyses$metrics[[1]][1]
+  metrics2 <- status$analyses$metrics[[1]][2]
 
 
   testthat::expect_identical(avail, expected = "Available")
