6e6cc60e83c92fa0cf62c30ec7784a804138e780
Liz Roten <liz.roten@metc.state.mn.us>
2023-12-26 15:47:53 -0600

    update message test

--- tests/testthat/test-create_tag.R
+++ tests/testthat/test-create_tag.R
@@ -4,5 +4,5 @@ test_that("create tag testing", {
     login_email = test_login,
     "streetlightR"
   ) %>%
-    testthat::expect_warning("Tag names must be case-insensitive unique.")
+    testthat::expect_warning("Tag names must be case-insensitive")
 })
