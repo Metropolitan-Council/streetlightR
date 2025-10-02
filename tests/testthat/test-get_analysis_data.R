d57761cea20724213517ca01d0bcb2b2d0c0ce59
Liz Roten <liz.roten@metc.state.mn.us>
2025-10-02 09:18:38 -0500

    increase sleep time

--- tests/testthat/test-get_analysis_data.R
+++ tests/testthat/test-get_analysis_data.R
@@ -7,7 +7,7 @@ testthat::test_that("VMT download is returned", {
     testthat::expect_error()
 })
 
-Sys.sleep(4)
+Sys.sleep(6)
 
 testthat::test_that("Correct VMT download is returned", {
   an_dat <- get_analysis_data(
