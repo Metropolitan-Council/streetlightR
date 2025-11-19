f80899801d7f39900ad4af0d87b1479ec16d2d8f
Liz Roten <liz.roten@metc.state.mn.us>
2025-11-19 15:09:24 -0600

    test OSM zone type upload

--- tests/testthat/test-upload_create_zone_set.R
+++ tests/testthat/test-upload_create_zone_set.R
@@ -26,3 +26,23 @@ testthat::test_that("Correct warning returned", {
   ) %>%
     testthat::expect_warning()
 })
+
+testthat::test_that("OSM set created", {
+  example_polygon <- sf::st_sfc(
+    sf::st_point(cbind(-93.09, 44.95)),
+    crs = 4326
+  ) %>%
+    sf::st_buffer(50)
+
+  polygon_response <- search_osm_ids(polygon = example_polygon)
+
+  Sys.sleep(3)
+
+
+  create_zone_set(
+    login_email = test_login,
+    osm_ids = list(polygon_response$data$osm_id) %>% unlist(),
+    zone_set_name = paste0("streetlightR-test-osm_upl5_", Sys.Date())
+  ) %>% 
+    testthat::expect_no_error()
+})
